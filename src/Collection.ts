485e10c99e4a8d5994a4af35a2c40c5bf197e736
Ba <ba@flygo.vn>
2024-12-18 21:48:26 +0700

    Update loading state

--- src/Collection.ts
+++ src/Collection.ts
@@ -1,7 +1,7 @@
 
 import { Subject, Subscription, Observable, merge, BehaviorSubject } from 'rxjs'
 import { LivequeryBaseEntity, QueryOption, QueryStream, Transporter, UpdatedData, Paging, Response } from '@livequery/types'
-import { bufferTime, filter, finalize, first, map, share, skip, tap, toArray } from 'rxjs/operators'
+import { bufferTime, filter, finalize, first, map, share, skip, takeUntil, tap, toArray } from 'rxjs/operators'
 
 export type LoadingIndicator = false | 'backward' | 'forward' | 'both'
 
@@ -80,28 +80,25 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
     this.collection_options.realtime = realtime
   }
 
-  #sync(stream: Array<QueryStream<T> & { ref: string }>, from_local: boolean = false, direction?: LoadingIndicator) {
+  #sync(stream: Array<QueryStream<T> & { ref: string }>, from: 'local' | 'request' | 'realtime', direction?: LoadingIndicator) {
+    if (from == 'realtime' && this.collection_options.realtime == false) return
     const state = this.getValue()
-    const realtime = this.collection_options.realtime ?? true
-    const actions = { update: false, reindex: false }
+    const actions = { reindex: false }
 
     for (const { data, error, code, message, } of stream) {
-
-      if (!from_local) {
+      if (from == 'request') {
         state.loading = false
-        actions.update = true
       }
+
       // Error & paging
       if (error) {
         state.error = true
         state.code = code
         state.message = message
-        actions.update = true
       }
 
       if (data?.summary) {
         state.summary = data.summary
-        actions.update = true
       }
 
 
@@ -116,28 +113,31 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
         if (index == -1 && type == 'added') {
           if (
             // Is first value from HTTP query
-            true
+            from == 'request'
             || (
               // Is realtime update that match filters
-              (realtime || from_local) && Object
+              from == 'realtime' && Object
                 .keys(state.options || {})
                 .filter(key => !key.includes('_'))
                 .every(key => {
+                  const [field, expression] = key.split(':')
+                  const a = payload[field as keyof typeof payload]
+                  const b = state.options?.[field as keyof QueryOption<T>]
+                  const map = {
+                    'default': () => a == b,
+                    eq: () => a == b,
+                    ne: () => a != b,
+                    lt: () => typeof a == 'number' && typeof b == 'number' && a < b,
+                    lte: () => typeof a == 'number' && typeof b == 'number' && a <= b,
+                    gt: () => typeof a == 'number' && typeof b == 'number' && a > b,
+                    gte: () => typeof a == 'number' && typeof b == 'number' && a >= b,
+                    in: () => Array.isArray(a) && a?.includes(b),
+                    like: () => typeof a == 'string' && a.includes(`${b}`),
+                    between: () => typeof a == 'number' && Array.isArray(b) && typeof b[0] == 'number' && b[0] <= a && typeof b[1] == 'number' && a <= b[1]
+                  }
                   try {
-                    const [field, expression] = key.split(':')
-                    const a = payload[field as keyof typeof payload] as number
-                    const b = state.options?.[field as keyof QueryOption<T>] as any as number
-                    if (!expression) return a == b
-                    if (expression == 'ne') return a != b
-                    if (expression == 'lt') return typeof a == 'number' && typeof b == 'number' && a < b
-                    if (expression == 'lte') return typeof a == 'number' && typeof b == 'number' && a <= b
-                    if (expression == 'gt') return typeof a == 'number' && typeof b == 'number' && a > b
-                    if (expression == 'gte') return typeof a == 'number' && typeof b == 'number' && a >= b
-                    if (expression == 'in' || expression == 'like') return Array.isArray(a) && a?.includes(b)
-                    if (expression == 'between') {
-                      const [x, y] = b as any as number[]
-                      return x <= a && a <= y
-                    }
+                    const fn = map[expression as keyof typeof map || 'default']
+                    if (fn) return fn()
                   } catch (e) { }
                   return false
                 })
@@ -160,14 +160,12 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
             direction == 'forward' ? state.items.push(item) : state.items.unshift(item)
 
             actions.reindex = true
-            actions.update = true
           }
         }
 
-        if (index >= 0 && (realtime || from_local)) {
+        if (index >= 0) {
 
           if (type == 'added' || type == 'modified') {
-            actions.update = true
             const sort_key_value_updated = this.#sorters.some(({ key }) => {
               const value = payload[key as keyof typeof payload]
               if (typeof value == 'string' || typeof value == 'number') return true
@@ -187,7 +185,6 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
 
 
           if (type == 'removed') {
-            actions.update = true
             state.items.splice(index, 1)
             for (const [document_id, i] of this.#IdMap) {
               i == index && this.#IdMap.delete(document_id)
@@ -263,7 +260,7 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
 
     }
 
-    actions.update && this.next(state)
+    this.next(state)
   }
 
 
@@ -273,9 +270,37 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
     loading: LoadingIndicator,
     flush: boolean = false
   ) {
+
+    if (flush) {
+      this.#pages.clear()
+      this.#queries.forEach(s => s.unsubscribe())
+      this.#queries.clear()
+      this.#IdMap.clear()
+    }
+
     if (!this.ref) return
     if (this.#refs.length == 0) return
-    if (this.getValue().loading) return
+    if (this.getValue().loading && !flush) return
+
+    const remain_data_refs = this.#refs.filter(ref => {
+      const paging = this.#pages.get(ref)
+      if (!paging) return true 
+      return loading == 'forward' ? paging.has?.next : paging.has?.prev
+    })
+
+    if (remain_data_refs.length == 0) return
+
+
+    this.next({
+      ... this.getValue(),
+      items: flush ? [] : this.getValue().items,
+      loading,
+      options: {
+        ... this.getValue().options || {},
+        ...options
+      } 
+    })
+
 
     this.collection_options.options = options
 
@@ -285,34 +310,6 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
       return { key, order }
     })
     this.#sorters.every(a => a.key != 'id') && this.#sorters.push({ key: 'id', order: -1 })
-
-    const state = {
-      ... this.getValue(),
-      items: flush ? [] : this.getValue().items,
-      loading,
-      options: {
-        ... this.getValue().options || {},
-        ...options
-      }
-    }
-
-    if (flush) {
-      this.#pages.clear()
-      this.#queries.forEach(s => s.unsubscribe())
-      this.#queries.clear()
-      this.#IdMap.clear()
-    }
-
-    const remain_data_refs = this.#refs.filter(ref => {
-      const paging = this.#pages.get(ref)
-      if (!paging) return true
-      return loading == 'forward' ? paging.has?.next : paging.has?.prev
-    })
-
-    const no_more_data = !flush && (remain_data_refs.length == 0 || this.getValue().loading)
-    if (no_more_data) return
-
-    this.next(state)
 
 
     const queries = remain_data_refs.map((ref, index) => {
@@ -342,13 +339,13 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
       ))
     ).pipe(
       toArray(),
-      tap(list => this.#sync(list, false, loading))
+      tap(list => this.#sync(list, 'request', loading))
     ).subscribe()
 
     const subscription = merge(...queries.map(q => q.pipe(skip(1)))).pipe(
       bufferTime(this.collection_options?.sync_delay || 500),
       filter(list => list.length > 0),
-      map(data => this.#sync(data, false, loading)),
+      map(data => this.#sync(data, 'realtime', loading)),
       finalize(() => first_values.unsubscribe())
     ).subscribe()
 
@@ -381,15 +378,16 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
 
 
   #find_ref_by_id(id: string | undefined | '' | false) {
-    if (!id || !this.ref) return { ref: this.ref, collection_ref: this.ref }
+    if (!id || !this.ref) return { ref: this.ref, collection_ref: this.ref, doc: null }
     const index = this.#IdMap.get(id)
     if (index == undefined) return {}
-    const origin_ref = this.getValue().items[index].__ref
+    const doc = this.getValue().items[index]
+    const origin_ref = doc.__ref
     if (!origin_ref) throw 'COLLECTION_REF_NOT_FOUND'
     const refs = origin_ref.split('/')
     const collection_ref = refs.slice(0, refs.length - (refs.length % 2 == 1 ? 0 : 1)).join('/')
     const ref = `${collection_ref}/${id}`
-    return { ref, id, collection_ref, index }
+    return { ref, id, collection_ref, index, doc }
   }
 
   public async add(payload: Partial<T>) {
@@ -401,7 +399,7 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
   }
 
   public async update({ id: update_payload_id, ...payload }: Partial<T>) {
-    const { id, ref } = this.#find_ref_by_id(update_payload_id as string)
+    const { id, ref, doc } = this.#find_ref_by_id(update_payload_id as string)
     if (!ref) return
     // Trigger local update
     this.#sync([{
@@ -413,7 +411,7 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
           type: 'modified'
         }]
       }
-    }], true)
+    }], 'local')
 
 
 
@@ -424,12 +422,12 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
         ref,
         data: {
           changes: [{
-            data: { id, __updating: false } as any,
+            data: { ...doc || {}, id, __updating: false } as any,
             ref,
             type: 'modified'
           }]
         }
-      }], true)
+      }], 'local')
       throw e
     }
   }
@@ -447,7 +445,7 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
           type: 'modified'
         }]
       }
-    }], true)
+    }], 'local')
 
     // Trigger  
     try {
@@ -462,7 +460,7 @@ export class CollectionObservable<T extends LivequeryBaseEntity = LivequeryBaseE
             type: 'modified'
           }]
         }
-      }], true)
+      }], 'local')
       throw e
     }
   }
